44468c271b741fdac56debca3872bd1c91636da0
itleo29 <itlei2966@gmail.com>
2026-07-28 11:44:21 +0900

    first commit

---
+++ src/test/java/org/jejuro/miraero/mock_server/MockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jejuro.miraero.mock_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
